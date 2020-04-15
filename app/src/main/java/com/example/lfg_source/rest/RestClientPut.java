61916bebe342a9394158cce1e724e0179c5b561a
se-na <nadine.sennhauser@hsr.ch>
2020-04-15 12:57:51 +0200

    put Method

--- app/src/main/java/com/example/lfg_source/rest/RestClientPut.java
+++ app/src/main/java/com/example/lfg_source/rest/RestClientPut.java
@@ -3,18 +3,14 @@ package com.example.lfg_source.rest;
 import android.os.AsyncTask;
 
 
-import com.example.lfg_source.entity.User;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 
 
-public class RestClientPut extends AsyncTask<String, Void, ResponseEntity<String>> {
+public class RestClientPut extends AsyncTask<String, Void, Void> {
 
     private boolean message;
 
@@ -27,7 +23,7 @@ public class RestClientPut extends AsyncTask<String, Void, ResponseEntity<String
     }
 
     @Override
-    protected ResponseEntity<String> doInBackground(String... uri) {
+    protected Void doInBackground(String... uri) {
         final String url = uri[0];
         RestTemplate restTemplate = new RestTemplate();
         try{
@@ -37,15 +33,10 @@ public class RestClientPut extends AsyncTask<String, Void, ResponseEntity<String
             HttpEntity<String> entity = new HttpEntity<String>(headers);
 
             restTemplate.put(url, message);
-            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-            return response;
         }
         catch (Exception e){
             String answer = e.getMessage();
-            return null;
         }
-    }
-    protected void onPostExecute(ResponseEntity<User[]> result){
-        HttpStatus statusCode = result.getStatusCode();
+        return null;
     }
 }
